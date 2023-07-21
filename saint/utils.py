9445d775f44bc7041db3682db1270488d0c2c332
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2023-07-21 19:27:07 +0900

    Update utils.py

--- saint/utils.py
+++ saint/utils.py
@@ -1,21 +1,12 @@
 import torch
-from sklearn.metrics import roc_auc_score, mean_squared_error, r2_score, accuracy_score, mean_absolute_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, mean_squared_log_error
 import numpy as np
-from augmentations import embed_data_mask
-import torch.nn as nn
 import pandas as pd
-
-def make_default_mask(x):
-    mask = np.ones_like(x)
-    mask[:,-1] = 0
-    return mask
-
-def tag_gen(tag,y):
-    return np.repeat(tag,len(y['data']))
 
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)  
+
 
 def get_scheduler(args, optimizer):
     if args.scheduler == 'cosine':
@@ -25,80 +16,24 @@ def get_scheduler(args, optimizer):
                                       milestones=[args.epochs // 2.667, args.epochs // 1.6, args.epochs // 1.142], gamma=0.1)
     return scheduler
 
-def imputations_acc_justy(model,dloader,device):
-    model.eval()
-    m = nn.Softmax(dim=1)
-    y_test = torch.empty(0).to(device)
-    y_pred = torch.empty(0).to(device)
-    prob = torch.empty(0).to(device)
-    with torch.no_grad():
-        for i, data in enumerate(dloader, 0):
-            x_categ, x_cont, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device)
-            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model)
-            reps = model.transformer(x_categ_enc, x_cont_enc)
-            y_reps = reps[:,model.num_categories-1,:]
-            y_outs = model.mlpfory(y_reps)
-            # import ipdb; ipdb.set_trace()   
-            y_test = torch.cat([y_test,x_categ[:,-1].float()],dim=0)
-            y_pred = torch.cat([y_pred,torch.argmax(m(y_outs), dim=1).float()],dim=0)
-            prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
-     
-    correct_results_sum = (y_pred == y_test).sum().float()
-    acc = correct_results_sum/y_test.shape[0]*100
-    auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
-    return acc, auc
+
+def embed_data(x_categ, x_cont, model):
+    device = x_cont.device
+    x_categ_enc = model.embeds(x_categ)
+    n1,n2 = x_cont.shape
+    _, n3 = x_categ.shape
+    if model.cont_embeddings == 'MLP':
+        x_cont_enc = torch.empty(n1,n2, model.dim)
+        for i in range(model.num_continuous):
+            x_cont_enc[:,i,:] = model.simple_MLP[i](x_cont[:,i])
+    else:
+        raise Exception('This case should not work!')    
+    #image_feature_enc = model.embeds(image_feature)
+    x_cont_enc = x_cont_enc.to(device)        
+    return x_categ, x_categ_enc, x_cont_enc#, image_feature_enc
 
 
-def multiclass_acc_justy(model,dloader,device):
-    model.eval()
-    vision_dset = True
-    m = nn.Softmax(dim=1)
-    y_test = torch.empty(0).to(device)
-    y_pred = torch.empty(0).to(device)
-    prob = torch.empty(0).to(device)
-    with torch.no_grad():
-        for i, data in enumerate(dloader, 0):
-            x_categ, x_cont, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device)
-            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model,vision_dset)
-            reps = model.transformer(x_categ_enc, x_cont_enc)
-            y_reps = reps[:,model.num_categories-1,:]
-            y_outs = model.mlpfory(y_reps)
-            # import ipdb; ipdb.set_trace()   
-            y_test = torch.cat([y_test,x_categ[:,-1].float()],dim=0)
-            y_pred = torch.cat([y_pred,torch.argmax(m(y_outs), dim=1).float()],dim=0)
-     
-    correct_results_sum = (y_pred == y_test).sum().float()
-    acc = correct_results_sum/y_test.shape[0]*100
-    return acc, 0
-
-
-def classification_scores(model, dloader, device, task,vision_dset):
-    model.eval()
-    m = nn.Softmax(dim=1)
-    y_test = torch.empty(0).to(device)
-    y_pred = torch.empty(0).to(device)
-    prob = torch.empty(0).to(device)
-    with torch.no_grad():
-        for i, data in enumerate(dloader, 0):
-            x_categ, x_cont, y_gts, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device),data[4].to(device)
-            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model,vision_dset)           
-            reps = model.transformer(x_categ_enc, x_cont_enc)
-            y_reps = reps[:,0,:]
-            y_outs = model.mlpfory(y_reps)
-            # import ipdb; ipdb.set_trace()   
-            y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
-            y_pred = torch.cat([y_pred,torch.argmax(y_outs, dim=1).float()],dim=0)
-            if task == 'binary':
-                prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
-     
-    correct_results_sum = (y_pred == y_test).sum().float()
-    acc = correct_results_sum/y_test.shape[0]*100
-    auc = 0
-    if task == 'binary':
-        auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
-    return acc.cpu().numpy(), auc
-
-def mean_sq_error(model, dloader, device, vision_dset):
+def mean_sq_error(model, dloader, device):
     model.eval()
     y_test = torch.empty(0).to(device)
     y_pred = torch.empty(0).to(device)
@@ -106,8 +41,8 @@ def mean_sq_error(model, dloader, device, vision_dset):
     y_test_lower = torch.empty(0).to(device)
     with torch.no_grad():
         for i, data in enumerate(dloader, 0):
-            x_categ, x_cont, y_gts, cat_mask, con_mask, image_feature, y_upper, y_lower = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device),data[4].to(device), data[5].to(device), data[6].to(device), data[7].to(device)
-            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model,vision_dset)           
+            x_categ, x_cont, y_gts, image_feature, y_upper, y_lower = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device),data[4].to(device), data[5].to(device)
+            _ , x_categ_enc, x_cont_enc = embed_data(x_categ, x_cont, model)           
             reps = model.transformer(x_categ_enc, x_cont_enc, image_feature)
             
             y_reps = reps[:,0,:]
@@ -126,10 +61,10 @@ def mean_sq_error(model, dloader, device, vision_dset):
         mae = mean_absolute_error(y_test.cpu(), y_pred.cpu())
         ## MSE
         mse = mean_squared_error(y_test.cpu(), y_pred.cpu())
-        ## RMSE _ 작을수록 좋음
+        ## RMSE 
         #rmse = mean_squared_error(y_test.cpu(), y_pred.cpu(), squared=False)
         rmsle = np.sqrt(mean_squared_log_error(y_test.cpu(), y_pred.cpu()))
-        ## R2 _ 1에 가까운 큰 수일수록 좋음
+        ## R2 
         r2 = r2_score(y_test.cpu(), y_pred.cpu())
         
         u = y_pred <= y_test_upper #u = torch.le(y_pred, y_test_upper).cpu() # <=
@@ -140,5 +75,3 @@ def mean_sq_error(model, dloader, device, vision_dset):
         ratio = sum(compare[compare['lower']==True]['upper']==True) / len(compare)
         
         return mae, mse, rmsle, r2, y_test, y_pred, ratio
-
-#y_test.cpu(), y_pred.cpu()
